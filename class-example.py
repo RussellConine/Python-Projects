47344869b2b671e67884b5ae1058020f227755b1
RussellConine <47286261+RussellConine@users.noreply.github.com>
2022-12-19 19:47:36 -0800

    formatting change

--- class-example.py
+++ class-example.py
@@ -5,6 +5,12 @@ class User:
     password = "123abcd"
     account = 0
 
+    def __init__(self, name, email, password, account):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.account = account
+
     # Define the methods of the class
     def login(self):
         entry_email = input("Enter your email: ")
@@ -13,7 +19,20 @@ class User:
             print("Welcome back, {}".format(self.name))
         else:
             print("You are not authorized for this page.")
+
+
+class Employee(User):
+    # create child class of User
+    base_pay = 11.00
+    department = 'General'
+
+class Customer(User):
+    # create child class of User
+    mailing_address = ' '
+    mailing_list = True
+
+
 # Outside of the class you would create an instance of the User class
-new_user = User()
+new_user = User("John Doe", "jdoe@gmail.com", "password123", "1234")
 # Call the login method using the new object
 new_user.login()
