198fd044c9f23cb25fc292bb04d98272917650f6
Russell Conine <47286261+RussellConine@users.noreply.github.com>
2023-01-04 12:30:26 -0800

    Update file_transfer.py

--- file_transfer.py
+++ file_transfer.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.filedialog
+import os
+import shutil
 
 class ParentWindow(Frame):
     def __init__(self,master):
@@ -9,7 +11,7 @@ class ParentWindow(Frame):
         self.master.title("File Transfer")
     
         # Creates button to select files from source directory
-        self.sourceDir_btn = Button(text="Select Source", width=20)
+        self.sourceDir_btn = Button(text="Select Source", width=20, command=self.sourceDir)
 
         # Position source button in GUI using grid
         self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
@@ -20,7 +22,7 @@ class ParentWindow(Frame):
         self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20, 10), pady=(30, 0))
 
         # Creates button to select destination of files from destination directory
-        self.destDir_btn = Button(text="Select Destination", width=20)
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
         # Positions destination button in GUI usng grid
         self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15,10))
 
@@ -29,9 +31,37 @@ class ParentWindow(Frame):
         # Positions entry in GUI using Grid
         self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20,10), pady=(15,10))
 
-        def sourceDir(self):
-            selectSourceDir = tkinter.filedialog.askdirectory()
-            # clear out the content that is inserted in the Entry widget
+        self.transfer_btn = Button(text = "Transfer Files", width=20, command=self.transferFiles)
+        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0, 15))
+
+        self.exit_btn = Button(text="Exit", width=20, command=self.exit_program)
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))
+
+    def sourceDir(self):
+        selectSourceDir = tkinter.filedialog.askdirectory()
+        # clear out the content that is inserted in the Entry widget
+        self.source_dir.delete(0, END)
+        self.source_dir.insert(0, selectSourceDir)
+
+    def destDir(self):
+        selectDestDir = tkinter.filedialog.askdirectory()
+        # clear out the content that is inserted in the Entry widget
+        self.destination_dir.delete(0, END)
+        self.destination_dir.insert(0,selectDestDir)
+
+    def transferFiles(self):
+        # get source directory
+        source = self.source_dir.get()
+        destination = self.destination_dir.get()
+        source_files = os.listdir(source)
+        for i in source_files:
+            shutil.move(source + "/" + i, destination)
+            print(i + ' was transferred')
+
+    def exit_program(self):
+        root.destroy()
+
+
             
 
 
